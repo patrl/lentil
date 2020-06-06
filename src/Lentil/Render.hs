e2e64045a52e75b8927c8e30fab8fde5f69aea14
Patrick Elliott <patrick.d.elliott@gmail.com>
2020-06-06 19:57:03 -0400

    Render module (using pandoc)

---
+++ src/Lentil/Render.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Lentil.Render where
+
+import           Lentil.Types
+import           Text.Pandoc             hiding ( Format )
+import qualified Data.Text                     as T
+import           System.FilePath
+import           Data.Char                      ( toLower )
+
+convertMarkdownToHtmlString :: T.Text -> IO T.Text
+convertMarkdownToHtmlString txt =
+  runIOorExplode $ readMarkdown def txt >>= writeHtml5String def
+
+formatFromExtension :: FilePath -> Format
+formatFromExtension f = case (map toLower $ takeExtension f) of
+  ".html"     -> HtmlFormat
+  ".xhtml"    -> HtmlFormat
+  ".markdown" -> MarkdownFormat
+  ".md"       -> MarkdownFormat
+  _           -> HtmlFormat
