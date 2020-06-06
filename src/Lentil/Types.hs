1f3431e29c55eece053eb24ae084dcff014b31e0
Patrick Elliott <patrick.d.elliott@gmail.com>
2020-06-06 19:56:43 -0400

    Type for page

--- src/Lentil/Types.hs
+++ src/Lentil/Types.hs
@@ -1,15 +1,21 @@
--- |
+{-# LANGUAGE DeriveGeneric #-}
 
 module Lentil.Types where
 
 import           Dhall
 
-data Site = Site {
-  siteTitle :: Text
-  , sourceDir :: [FilePath]
-  , dataDir :: [FilePath]
-  , filesDir :: [FilePath]
-  , deployDir :: FilePath
-  , defaultLayout :: FilePath
-  , indexFile :: FilePath
-                 } deriving (Show, Read, Eq)
+data Site = Site { siteTitle :: Text
+                 , outputFolder :: Text
+                 , dataDir :: Text
+                 , defaultLayout :: Text
+                 } deriving (Show, Read, Eq, Generic)
+
+data Page = Page { pageTitle :: Text,
+                   contents :: Text,
+                   style :: Text } deriving (Show, Read, Eq, Generic)
+
+instance FromDhall Site
+instance FromDhall Page
+instance ToDhall Page
+
+data Format = HtmlFormat | MarkdownFormat deriving (Eq,Show,Read)
