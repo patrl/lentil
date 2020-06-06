11e7ab685c85ce5cd8253614deb3fd99472aa531
Patrick Elliott <patrick.d.elliott@gmail.com>
2020-06-06 19:56:32 -0400

    App now copies static files

--- src/Main.hs
+++ src/Main.hs
@@ -5,9 +5,34 @@
 module Main where
 
 import Dhall
-import Lentil.Types()
+import Lentil.Types
+import Lentil.Render
+import Lentil.Shake
+import qualified Data.Text.IO as T
+import Development.Shake
+import Development.Shake.Forward
 
 main :: IO ()
 main = do
-  (siteRaw :: Text) <- input auto "./data/site.dhall"
-  print siteRaw
+ 
+  let shOpts = forwardOptions $ shakeOptions { shakeVerbosity = Chatty, shakeLintInside = ["\\"]}
+ 
+  deployFolder <- input auto "(./data/config.dhall).outputFolder" :: IO FilePath -- read the output folder from the global dhall filepath
+  staticFiles <- input auto "(./data/config.dhall).dataDir" :: IO FilePath
+ 
+  print deployFolder
+  readExample <- T.readFile "./data/index.md"
+  excontents <- convertMarkdownToHtmlString readExample
+  -- siteConfig <- input auto "./data/config.dhall" :: IO Site -- Read in the global site configuration from config.dhall
+  pagetitle <- input auto "(./data/config.dhall).siteTitle"
+  pageInfo <- return $ Page { pageTitle = pagetitle, contents = excontents, style = "css/gruvbox.css" }
+  pageTemplate <- input auto "./data/layout.dhall" :: IO (Page -> Text)
+  templated <- return $ pageTemplate pageInfo
+
+  -- print (siteConfig :: Site)
+  -- print (title :: Text)
+  -- print pageInfo
+  -- print templated
+  print templated
+
+  shakeArgsForward shOpts $ copyStaticFiles (staticFiles <> "css") deployFolder
