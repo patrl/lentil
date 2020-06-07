d477aeb7219e5b90312af5776caed7b5834995d5
Patrick Elliott <patrick.d.elliott@gmail.com>
2020-06-07 10:52:32 -0400

    Added build page function

--- src/Lentil/Shake.hs
+++ src/Lentil/Shake.hs
@@ -3,9 +3,13 @@ module Lentil.Shake where
 import Development.Shake
 import Development.Shake.FilePath
 import Control.Monad
+import Lentil.Types
 
 copyStaticFiles :: FilePath -> FilePath -> Action ()
 copyStaticFiles cssDir outputDir = do
   cssFiles <- getDirectoryFiles cssDir [ "//*.css" ]
   void $ forP cssFiles $ \filepath ->
     copyFileChanged (cssDir </> filepath) (outputDir </> filepath)
+
+buildPage :: FilePath -> Action Page
+buildPage = undefined
