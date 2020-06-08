0349b52f64d5a81a7f868ef0411779cc7ca2d74a
Patrick Elliott <patrick.d.elliott@gmail.com>
2020-06-07 21:51:26 -0400

    update pandoc helpers

--- src/Lentil/Pandoc.hs
+++ src/Lentil/Pandoc.hs
@@ -3,21 +3,27 @@
 module Lentil.Pandoc where
 
 import           Text.Pandoc
--- import           Development.Shake
--- import qualified Data.Map as M
-import qualified Data.Text as T
+import           Development.Shake
+import qualified Data.Text                     as T
 
-defaultMarkdownOptions :: ReaderOptions
-defaultMarkdownOptions = def { readerExtensions = exts }
+defaultReaderOptions :: ReaderOptions
+defaultReaderOptions = def { readerExtensions = mconcat [pandocExtensions] }
+
+defaultWriterOptions :: WriterOptions
+defaultWriterOptions = def { writerExtensions = exts }
  where
   exts = mconcat
     [ extensionsFromList
-      [ Ext_yaml_metadata_block
-      , Ext_fenced_code_attributes
-      , Ext_auto_identifiers
-      ]
-    , githubMarkdownExtensions
+        [ Ext_auto_identifiers
+        , Ext_native_divs
+        , Ext_line_blocks
+        , Ext_native_spans
+        ]
     ]
 
-titleToHtml5 :: PandocMonad m => Meta -> m T.Text
-titleToHtml5 = writeHtml5String def . Pandoc mempty . mempty . docTitle
+-- Handle possible pandoc failure within IO and lift the result into the Action monad.
+pandocIOToAction :: PandocIO a -> Action a
+pandocIOToAction = liftIO . runIOorExplode
+
+renderMd :: T.Text -> Action (T.Text)
+renderMd txt =  pandocIOToAction $ (readMarkdown defaultReaderOptions txt) >>= writeHtml5String defaultWriterOptions
