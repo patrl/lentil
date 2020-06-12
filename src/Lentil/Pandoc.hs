31343dbe923287fa2a5d1ff957afed4b68932fb6
Patrick Elliott <patrick.d.elliott@gmail.com>
2020-06-12 19:00:01 -0400

    Some refactoring

--- src/Lentil/Pandoc.hs
+++ src/Lentil/Pandoc.hs
@@ -3,14 +3,14 @@
 module Lentil.Pandoc where
 
 import           Text.Pandoc
-import           Development.Shake
+import           Development.Shake (liftIO,Action)
 import qualified Data.Text                     as T
 
 defaultReaderOptions :: ReaderOptions
 defaultReaderOptions = def { readerExtensions = mconcat [pandocExtensions] }
 
 defaultWriterOptions :: WriterOptions
-defaultWriterOptions = def { writerExtensions = exts }
+defaultWriterOptions = def { writerExtensions = exts, writerHTMLMathMethod = MathML }
  where
   exts = mconcat
     [ extensionsFromList
@@ -23,14 +23,32 @@ defaultWriterOptions = def { writerExtensions = exts }
 
 -- Handle possible pandoc failure within IO and lift the result into the Action monad.
 -- We want to handle errors within the IO monad.
-pandocIOToAction :: PandocIO a -> Action a
-pandocIOToAction = liftIO . runIOorExplode
+liftPandoc :: PandocIO a -> Action a
+liftPandoc = liftIO . runIOorExplode
 
+-- Note that this drops any associated metadata
 renderMd :: T.Text -> Action (T.Text)
-renderMd txt =  pandocIOToAction $ (readMarkdown defaultReaderOptions txt) >>= writeHtml5String defaultWriterOptions
+renderMd txt =  liftPandoc $ do
+  (Pandoc _ body) <- myReadMarkdown txt
+  myWriteHtml5String (Pandoc mempty body)
+
+myReadOrg :: T.Text -> PandocIO Pandoc
+myReadOrg = readOrg defaultReaderOptions
 
 myReadMarkdown :: T.Text -> PandocIO Pandoc
 myReadMarkdown = readMarkdown defaultReaderOptions
 
 myWriteHtml5String :: Pandoc-> PandocIO T.Text
 myWriteHtml5String = writeHtml5String defaultWriterOptions
+
+getMeta :: Pandoc -> Meta
+getMeta (Pandoc meta _) = meta
+
+metaToHtmlTitle :: Meta -> Action (T.Text)
+metaToHtmlTitle =
+  liftPandoc
+    . myWriteHtml5String
+    . Pandoc mempty
+    . pure
+    . Plain
+    . docTitle
