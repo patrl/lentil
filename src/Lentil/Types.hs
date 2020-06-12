ab4be165b365bb96fbfdc752e024b6a24e40ce0e
Patrick Elliott <patrick.d.elliott@gmail.com>
2020-06-12 18:59:40 -0400

    Dhall instances now derived automatically

--- src/Lentil/Types.hs
+++ src/Lentil/Types.hs
@@ -1,27 +1,26 @@
-{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveGeneric,DeriveAnyClass #-}
 
 module Lentil.Types where
 
 import           Dhall
 
+-- This should correspond to the dhall datatype in config.dhall
 data Site = Site { siteTitle :: Text
                  , outputFolder :: Text
                  , dataDir :: Text
                  , defaultLayout :: Text
-                 } deriving (Show, Read, Eq, Generic)
+                 } deriving (Show, Read, Eq, Generic, FromDhall)
 
 data Page = Page { pageTitle :: Text,
                    contents :: Text,
                    style :: Text,
-                   date :: Text } deriving (Show, Read, Eq, Generic)
+                   date :: Text } deriving (Show, Read, Eq, Generic, FromDhall, ToDhall)
+
+data PageMeta = PageMeta { metaTitle :: Text, metaStyle :: Text } deriving  (Show,Read,Eq,Generic,FromDhall)
 
 data SitePage = SitePage {
   title :: Text
   , pageContents :: Text
   }
 
-instance FromDhall Site
-instance FromDhall Page
-instance ToDhall Page
-
-data Format = HtmlFormat | MarkdownFormat deriving (Eq,Show,Read)
+data Format = HtmlFormat | MarkdownFormat | OrgFormat deriving (Eq,Show,Read)
