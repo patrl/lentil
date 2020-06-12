ddcf2b94d8e5701a0d114719ad983e0bd2f18c23
Patrick Elliott <patrick.d.elliott@gmail.com>
2020-06-12 18:59:51 -0400

    Can now choose which port to serve at

--- src/Lentil/Serve.hs
+++ src/Lentil/Serve.hs
@@ -5,5 +5,5 @@ module Lentil.Serve (serveSite) where
 import           Network.Wai.Handler.Warp       ( run )
 import           Network.Wai.Application.Static
 
-serveSite :: FilePath -> IO ()
-serveSite dir = run 8000 (staticApp (defaultFileServerSettings dir))
+serveSite :: FilePath -> Int -> IO ()
+serveSite dir port = run port (staticApp (defaultFileServerSettings dir))
